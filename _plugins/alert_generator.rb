c2eb9b2dceee013230b4c6fb107277bb5b805204
unknown <idjemere@idjemere.telerik.com>
2015-03-20 16:48:00 +0200

    Fixed: alerts to support multiple paragraphs, lists and all types by specs

--- _plugins/alert_generator.rb
+++ _plugins/alert_generator.rb
@@ -7,16 +7,29 @@ module Reading
 			createAlert("tip", p.content)
 			createAlert("important", p.content)
 			createAlert("caution", p.content)
+			createAlert("warning", p.content)
+			createAlert("note", p.content)
 		end
     end
 	
 	def createAlert(alertType, content)
-		sub_string = content.scan(/(>#{alertType})(.*)/)		  
+		# original code (matches only only one line): 
+		# sub_string = content.scan(/(>#{alertType})(.*)/)	
+		
+		# FIX: match entire blockquote 
+		sub_string = content.scan(/(>#{alertType})((.|\n[\w\*\[!<>|#])*)/)
 		if sub_string.count == 0
 			##puts "no " + alertType + "s"
 		else
-			sub_string.each do |s|			
-				block ="<blockquote class='#{alertType}'>" +  @converter.convert(s[1]) + "</blockquote>"
+			sub_string.each do |s|		
+				# Note, there should be a capitalize rule for the label <span class='label'>#{alertType}</span> - text-transform: capitalize;	
+
+				# [Multiple paragraphs issue] Solution 1 (using blockqoute): between <para> elements, a <br style="display:none;"/> to be added 	
+				# block ="<blockquote class='#{alertType}'><span class='label'>#{alertType}</span><hr/>" +  @converter.convert(s[1]) + "</blockquote>"
+				
+				# [Multiple paragraphs issue] Solution 2 (using div wrapper): between <para> elements, a > character to be added. 
+				block ="<div class='alert #{alertType}'><span class='label'>#{alertType}</span><hr/>" +  @converter.convert("> " + s[1]) + "</div>"
+				
 				slugsInBlock = block.scan(/.*?(%7[Bb]%slug%20([\w-]+)%{2}7[Dd])/)				
 				if	slugsInBlock.count > 0				
 					slugsInBlock.each do |slug|								
