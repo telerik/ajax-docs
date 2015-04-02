4c417b0a14d20ef48a9f9961b69051799a5c31f9
joneff <joneff@gmail.com>
2015-04-02 16:05:47 +0300

    Improve styling of documentation app to match requirements

--- _plugins/alert_generator.rb
+++ _plugins/alert_generator.rb
@@ -1,44 +1,48 @@
 module Reading
   class AlertGenerator < Jekyll::Generator
-    def generate(site)	
+    def generate(site)
 		@site = site
-	    @converter = site.getConverterImpl(Jekyll::Converters::Markdown)	
-        site.pages.each do |p|			
+	    @converter = site.getConverterImpl(Jekyll::Converters::Markdown)
+        site.pages.each do |p|
+        	# green
 			createAlert("tip", p.content)
-			createAlert("important", p.content)
-			createAlert("caution", p.content)
-			createAlert("warning", p.content)
+			# blue
 			createAlert("note", p.content)
+			# orange
+			createAlert("important", p.content)
+			createAlert("warning", p.content)
+			# red
+			createAlert("caution", p.content)
 		end
     end
-	
+
 	def createAlert(alertType, content)
-		# original code (matches only only one line): 
-		# sub_string = content.scan(/(>#{alertType})(.*)/)	
-		
-		# FIX: match entire blockquote 
+		# original code (matches only only one line):
+		# sub_string = content.scan(/(>#{alertType})(.*)/)
+
+		# FIX: match entire blockquote
 		sub_string = content.scan(/(>#{alertType})((.|\n[\w\*\[!<>|#])*)/)
 		if sub_string.count == 0
 			##puts "no " + alertType + "s"
 		else
-			sub_string.each do |s|		
-				# Note, there should be a capitalize rule for the label <span class='label'>#{alertType}</span> - text-transform: capitalize;	
+			sub_string.each do |s|
+				# Note, there should be a capitalize rule for the label <span class='label'>#{alertType}</span> - text-transform: capitalize;
 
-				# [Multiple paragraphs issue] Solution 1 (using blockqoute): between <para> elements, a <br style="display:none;"/> to be added 	
+				# [Multiple paragraphs issue] Solution 1 (using blockqoute): between <para> elements, a <br style="display:none;"/> to be added
 				# block ="<blockquote class='#{alertType}'><span class='label'>#{alertType}</span><hr/>" +  @converter.convert(s[1]) + "</blockquote>"
-				
-				# [Multiple paragraphs issue] Solution 2 (using div wrapper): between <para> elements, a > character to be added. 
+
+				# [Multiple paragraphs issue] Solution 2 (using div wrapper): between <para> elements, a > character to be added.
 				block ="<div class='alert #{alertType}'><span class='label'>#{alertType}</span><hr/>" +  @converter.convert("> " + s[1]) + "</div>"
-				
-				slugsInBlock = block.scan(/.*?(%7[Bb]%slug%20([\w-]+)%{2}7[Dd])/)				
-				if	slugsInBlock.count > 0				
-					slugsInBlock.each do |slug|								
-						targetPage = @site.pages.find {|p| p.data['slug'] == slug[1]}			
+
+				slugsInBlock = block.scan(/.*?(%7[Bb]%slug%20([\w-]+)%{2}7[Dd])/)
+				if	slugsInBlock.count > 0
+					slugsInBlock.each do |slug|
+						targetPage = @site.pages.find {|p| p.data['slug'] == slug[1]}
 						if targetPage
-							block.sub!(slug[0], targetPage.url.sub('.html', ''))					
+							block.sub!(slug[0], targetPage.url.sub('.html', ''))
 						end
 					end
-				end				
+				end
 				content.sub!(s[0]+s[1], block)
 			end
 		end
