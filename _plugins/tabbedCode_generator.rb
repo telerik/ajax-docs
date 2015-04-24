64bcf91461c81e9b28f3894ca9215d9398525c5a
unknown <idjemere@idjemere.telerik.com>
2015-04-24 16:00:11 +0300

    Add error handling when tabbedCode_generator fails due to no empty lines below.

--- _plugins/tabbedCode_generator.rb
+++ _plugins/tabbedCode_generator.rb
@@ -4,11 +4,11 @@ module Reading
 			@site = site
 			@converter = site.getConverterImpl(Jekyll::Converters::Markdown)	
 			site.pages.each do |p|			
-				createTabbedCode(p.content)
+				createTabbedCode(p, p.content)
 			end
 		end
 	
-		def createTabbedCode(content)
+		def createTabbedCode(page, content)
 			tab_start = /````\w/
 			tab_end = /````\s{2,}/
 
@@ -17,14 +17,17 @@ module Reading
 			indexes = []
 			
 			while first_index && !indexes.include?(first_index)
+				if last_index.nil? 
+					Jekyll.logger.warn "Tabbed Code Error:", "Failed to generate tabbed code in #{page.path}" 
+				end
+					
 				indexes.push(first_index)
 				block = encode_liquid(content[first_index..last_index + 4])
 				block = @converter.convert(block) 
-
 				content[first_index..last_index + 4] = "<div class='tabbedCode'>" + block + "</div>"
 
 				first_index = content.index(tab_start, last_index)
-				last_index = first_index && content.index(tab_end, last_index + 4)
+ 				last_index = first_index && content.index(tab_end, last_index + 4)
 			end
 		end
 		
