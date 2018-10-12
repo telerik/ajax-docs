7eedf60ee16e8a13d4e1c629f11d9afbf2ebf7cc
imtodor <tmitev89@gmail.com>
2018-10-12 13:29:21 +0300

    Revert "chore: Removing unnecessary plugins"
    This reverts commit 34efbeadcfe5633435c899fae8eb67f961c33193.

--- _plugins/navigation_generator.rb
+++ _plugins/navigation_generator.rb
@@ -1,85 +1,84 @@
 module Jekyll
 
-  class NavigationGenerator < Generator
-      def initialize(config)
-          @navigation = Hash[(config['navigation'] || {}).map { |key, value| [/^#{key.gsub('*', '.*?')}$/, value] }]
-          @exclude_navigation = (config['exclude_navigation'] || {}).map { |item| /^#{item.gsub('*', '.*?')}$/ }
-      end
+    class NavigationGenerator < Generator
+        def initialize(config)
+            @navigation = Hash[(config['navigation'] || {}).map { |key, value| [/^#{key.gsub('*', '.*?')}$/, value] }]
+        end
 
-      def categories(site)
-          categories = {}
+        def categories(site)
+            categories = {}
 
-          site.pages.each do |page|
-              category = page.data['category']
+            site.pages.each do |page|
+                category = page.data['category']
 
-              next if page.data['publish'] == false || page.data['include_in_navigation'] == false
-              next unless category
+                next if page.data['publish'] == false
+                next unless category
 
-              node = categories[category]
+                node = categories[category]
 
-              unless node
-                  categories[category] = node = []
-              end
+                unless node
+                    categories[category] = node = []
+                end
 
-              url = page.url.sub('/', '')
+                url = page.url.sub('/', '')
+				
+				#exclude MOSS 2007 documentation from the TOC until it is deleted
+				next if url.include? "sharepoint/2007/"
+				
+                segments = url.split('/')
 
-              next if @exclude_navigation.any? { |regex| regex.match(url) }
+                segments.each_with_index do |segment, index|
+                    item = node.find { |n| n['path'] == segment }
 
-              segments = url.split('/')
+                    unless item
 
-              segments.each_with_index do |segment, index|
-                  item = node.find { |n| n['path'] == segment }
+                        item = { 'path' => segment }
 
-                  unless item
-                      item = { 'path' => segment }
+                        if index == segments.size - 1
+                            item['position'] = page.data['position'] if page.data['position']
+                            item['text'] = page.data['title']
+                            item['spriteCssClass'] = 'article'
+                        else
+                            path = segments[0..index].join('/')
+                            navigation_entry =  @navigation.find { |key, value| path =~ key }
+                            mapping = navigation_entry ? navigation_entry[1] : {}
+                            item['text'] = mapping['title'] || segment
+                            item['items'] = []
+                            item['position'] = mapping['position'] if mapping.has_key?('position')
+                        end
 
-                      if index == segments.size - 1
-                          item['position'] = page.data['position'] if page.data['position']
-                          item['text'] = page.data['title']
-                          item['spriteCssClass'] = 'article'
-                          item['isNew'] = page.data['isNew'] if page.data['isNew']
-                      else
-                          path = segments[0..index].join('/')
-                          navigation_entry =  @navigation.find { |key, value| path =~ key }
-                          mapping = navigation_entry ? navigation_entry[1] : {}
-                          item['text'] = mapping['title'] || segment
-                          item['items'] = []
-                          item['isNew'] = mapping['isNew'] if mapping.has_key?('isNew')
-                          item['position'] = mapping['position'] if mapping.has_key?('position')
-                      end
+                        node << item
+                    end
 
-                      node << item
-                  end
+                    node = item['items']
 
-                  node = item['items']
+                end
+            end
 
-              end
-          end
+            categories.each {  |key, value| sort!(value) }
 
-          categories.each {  |key, value| sort!(value) }
+            categories
+        end
 
-          categories
-      end
+        def generate(site)
+            categories(site).each do |key, value|
+                filename = "#{key}.json"
 
-      def generate(site)
-          categories(site).each do |key, value|
-              filename = "#{key}.json"
+                FileUtils.mkdir_p(site.dest) unless File.exist?(site.dest)
 
-              FileUtils.mkdir_p(site.dest) unless File.exist?(site.dest)
+                File.write(File.join(site.dest, filename), value.to_json)
 
-              File.write(File.join(site.dest, filename), value.to_json)
+                # Keep the file from being cleaned by Jekyll
+                site.keep_files << filename
+            end
+        end
 
-              # Keep the file from being cleaned by Jekyll
-              site.keep_files << filename
-          end
-      end
+        def sort!(items)
+            items.each {|item| sort!(item['items']) if item['items'] }
 
-      def sort!(items)
-          items.each {|item| sort!(item['items']) if item['items'] }
+            # sorty by position, directory or file and then title (ignoring case)
+            items.sort_by! {|a| [a['position'] || 1000000, a.has_key?('items') ? -1 : 1,  a['text'].downcase]}
+        end
 
-          # sorty by position, directory or file and then title (ignoring case)
-          items.sort_by! {|a| [a['position'] || 1000000, a.has_key?('items') ? -1 : 1,  a['text'].nil? ? a['path'] : a["text"].downcase]}
-      end
-
-  end
+    end
 end
